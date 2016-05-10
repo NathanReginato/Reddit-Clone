97d49b980dca670a58eed6efcb2081031061e8b9
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-10 13:49:19 -0600

    Can serve JSON from server side

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/v1/posts', function(req, res, next) {
-  res.json("{'stuff':'things'}")
+  res.json({stuff:'things'})
 });
 
 module.exports = router;
