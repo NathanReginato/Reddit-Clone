9fcbe0c3fc4c56ee3c0335740d8c59f332d86ae0
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-10 13:46:57 -0600

    Initial server commit

---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/v1/posts', function(req, res, next) {
+  res.json("{'stuff':'things'}")
+});
+
+module.exports = router;
