c270173d0f41a12faa166025cb5d605f6d9d46c2
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-11 10:43:00 -0600

    Server call returns object with minimal db calls

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,17 +2,26 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knexconfig.js')
 
+
 /* GET users listing. */
 router.get('/v1/posts', function(req, res, next) {
   knex('reddit-posts').then(function(posts){
-    res.json(posts)
-  })
-});
-
-router.get('/v1/comment', function(req, res, next) {
-  knex('reddit-comments').then(function(comments){
-    res.json(comments)
-  })
+    return knex('reddit-comments').then(function(comments){
+      var postObject = [];
+      posts.forEach(function(post){
+        var pTemp;
+        var cTemp = [];
+        pTemp = post;
+        comments.forEach(function(comment){
+          if (comment.post_id === post.id) {
+            cTemp.push(comment);
+          }
+        });
+        postObject.push({'post':pTemp, 'comment':cTemp});
+      });
+      res.json(postObject);
+    });
+  });
 });
 
 module.exports = router;
