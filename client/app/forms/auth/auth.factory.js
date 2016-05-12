becce5f7d593331e5abec1a639607c8132585749
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-11 22:58:52 -0600

    Get auth data on server side

--- client/app/forms/auth/auth.factory.js
+++ client/app/forms/auth/auth.factory.js
@@ -14,27 +14,24 @@
     }
 
     function signup(formValues) {
-      console.log(formValues);
-      // return $http.post('http://localhost:3000/api/v1/signup', formValues)
-      // .then(function(res) {
-      //
-      // })
+      return $http.post('http://localhost:3000/auth/signup', formValues)
+      .then(function(res) {
+
+      })
     }
 
     function login(formValues) {
-      console.log(formValues);
-      // return $http.post('http://localhost:3000/api/v1/login', formValues)
-      // .then(function(res) {
-      //
-      // })
+      return $http.post('http://localhost:3000/auth/login', formValues)
+      .then(function(res) {
+
+      })
     }
 
     function logout(formValues) {
-      console.log(formValues);
-      // return $http.post('http://localhost:3000/api/v1/logout', formValues)
-      // .then(function(res) {
-      //
-      // })
+      return $http.post('http://localhost:3000/auth/logout', formValues)
+      .then(function(res) {
+
+      })
     }
   }
 })();
