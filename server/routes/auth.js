5d51fce0fb0efb257ba5778750ab18f2c39722db
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-11 22:33:20 -0600

    Commented out bcrypt and added logs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,32 +4,37 @@ var bcrypt = require('bcrypt');
 var knex = require('../db/knexconfig.js')
 
 router.post('/login', function(req, res, next) {
-  knex('users')
-  .where('user_name', '=', req.body.username.toLowerCase())
-  .first()
-  .then(function(response){
-    if(response && bcrypt.compareSync(req.body.password, response.bcrypt_hash)){
-      var vpassport = { user: { id: response.user_id} };
-      req.session.passport = vpassport
-      res.redirect('/admin/'+ response.user_id +'/home');
-    } else {
-      res.render('/');
-    }
-  });
+  console.log('login');
+  console.log(req.body);
+  // knex('users')
+  // .where('user_name', '=', req.body.username.toLowerCase())
+  // .first()
+  // .then(function(response){
+  //   if(response && bcrypt.compareSync(req.body.password, response.bcrypt_hash)){
+  //     var vpassport = { user: { id: response.user_id} };
+  //     req.session.passport = vpassport
+  //     res.redirect('/admin/'+ response.user_id +'/home');
+  //   } else {
+  //     res.render('/');
+  //   }
+  // });
 });
 
 router.post('/signup', function(req, res, next) {
-  var hash = bcrypt.hashSync(req.body.password, 8);
-  knex('users')
-  .insert({'user_name': req.body.username.toLowerCase(), 'bcrypt_hash': hash, 'picture_url': req.body.picture_url})
-  .then(function(response){
-    res.redirect('/');
-  })
+  console.log('signup');
+  console.log(req.body);
+  // var hash = bcrypt.hashSync(req.body.password, 8);
+  // knex('users')
+  // .insert({'user_name': req.body.username.toLowerCase(), 'bcrypt_hash': hash, 'picture_url': req.body.picture_url})
+  // .then(function(response){
+  //   res.redirect('/');
+  // })
 });
 
 router.get('/logout', function(req,res,next){
-  req.session = null;
-  res.redirect('/');
+  console.log('logout');
+  // req.session = null;
+  // res.redirect('/');
 });
 
 module.exports = router;
