4e7f3b61b7a13a9ade2dbf415dd0483cbbd4e754
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-12 21:37:17 -0600

    Firebase config changed to allow html 5 mode

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,18 +6,18 @@ var knex = require('../db/knexconfig.js')
 router.post('/login', function(req, res, next) {
   console.log('login');
   console.log(req.body);
-  // knex('users')
-  // .where('user_name', '=', req.body.username.toLowerCase())
-  // .first()
-  // .then(function(response){
-  //   if(response && bcrypt.compareSync(req.body.password, response.bcrypt_hash)){
-  //     var vpassport = { user: { id: response.user_id} };
-  //     req.session.passport = vpassport
-  //     res.redirect('/admin/'+ response.user_id +'/home');
-  //   } else {
-  //     res.render('/');
-  //   }
-  // });
+  knex('reddit-users')
+  .where('user_name', '=', req.body.username.toLowerCase())
+  .first()
+  .then(function(response){
+    if(response && bcrypt.compareSync(req.body.password, response.bcrypt_hash)){
+      var vpassport = { user: { id: response.user_id} };
+      req.session.passport = vpassport
+      res.redirect('/admin/'+ response.user_id +'/home');
+    } else {
+      res.render('/');
+    }
+  });
 });
 
 router.post('/signup', function(req, res, next) {
