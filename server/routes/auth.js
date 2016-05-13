6c3607528efabb82482d86dace92360a900a8f61
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-13 14:14:04 -0600

    Braking up auth routes into files

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/signup', function(req, res, next) {
-  res.json({'stuff':'things'})
-});
+// router.use('/auth/signup.js');
+// router.use('/auth/login.js');
+// router.use('/auth/logout.js');
 
 module.exports = router;
--- server/routes/auth/signup.js
+++ server/routes/auth/signup.js
@@ -5,19 +5,18 @@ var knex = require('../../db/knexconfig.js')
 var jwt = require('jsonwebtoken')
 
 router.post('/signup', function(req, res, next) {
-  validate(req, next)
-  console.log('validated');
+  res.json({'stuff':'things'})
 });
 
 
-function validate(req, next) {
-
-  const errors = [];
-
-  if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
-  if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
-
-  next();
-}
+// function validate(req, next) {
+//
+//   const errors = [];
+//
+//   if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+//   if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+//
+//   next();
+// }
 
 module.exports = router;
