74682e8134f27114d897101852b0d29cbdbe8499
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-13 15:46:56 -0600

    Server side jwt and bcrypt works on sign in

--- server/routes/auth/signup.js
+++ server/routes/auth/signup.js
@@ -29,6 +29,30 @@ function insertUser(req, res, next) {
     .count()
     .first()
     .then(function(result){
-    })
+      if (result.count === "0") {
+        const saltRounds = 4;
+        const passwordHash = bcrypt.hashSync(req.body.password, saltRounds)
+        knex('reddit-users').insert({
+            email: req.body.email,
+            password: passwordHash
+          })
+          .returning('*')
+          .then(function (users) {
+            const user = users[0]
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+
+              res.status(200).json({
+                id: user.id,
+                email: user.email,
+                token: token
+              })
+          })
+        } else {
+          res.status(422).json({
+            errors: ["Email has already been taken"]
+          })
+        }
+      })
 }
+
 module.exports = router;
