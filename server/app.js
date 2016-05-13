0de40da9f7112bc045ba244b3e903b0ff2909a68
Nathan Reginato <nathan.reginato@gmail.com>
2016-05-13 11:49:13 -0600

    Auth reorgainzed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var jwt = require('express-jwt');
 
 var api = require('./routes/api');
 var auth = require('./routes/auth');
---
+++ server/routes/auth/signup.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var knex = require('../../db/knexconfig.js')
+var jwt = require('jsonwebtoken')
+
+router.post('/signup', function(req, res, next) {
+  validate(req, next)
+  console.log('validated');
+});
+
+
+function validate(req, next) {
+
+  const errors = [];
+
+  if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+  if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+
+  next();
+}
+
+module.exports = router;
